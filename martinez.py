8752d0db0fd1e98f93f8d1b188ddcfffdf9b0b2b
unknown <nickinajafi1999@gmail.com>
2023-04-30 21:43:20 -0400

    working version_1

---
+++ martinez.py
@@ -0,0 +1,96 @@
+import os
+import pandas as pd
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem
+from PyQt5.QtCore import Qt
+
+# clear the terminal
+os.system('cls' if os.name == 'nt' else 'clear')
+df = pd.read_csv('output_3.csv',nrows=1395,usecols=['Procedure, localization, technique', 'Surgeon', 'Delay'])
+df['Procedure, localization, technique'] = df['Procedure, localization, technique'].str.split(',').str[0]
+df = df.rename(columns={'Procedure, localization, technique': 'Procedure'})
+# Create a new dataframe with only the rows that contain 'Jennifer'
+df_jennifer = df[df['Surgeon'] == 'Martinez, Adriana']
+print(df_jennifer)
+df_jennifer_procedure_total_hip = df_jennifer[df_jennifer['Procedure'].str.contains('Total hip')]
+df_jennifer_procedure_total_knee = df_jennifer[df_jennifer['Procedure'].str.contains('Total knee')]
+df_jennifer_procedure_total_shoulder = df_jennifer[df_jennifer['Procedure'].str.contains('Total shoulder')]
+
+# Output the new dataframe Jennifer Mutch
+jennifer_total_shoulder_count = len(df_jennifer_procedure_total_shoulder)
+jennifer_total_knee_count = len(df_jennifer_procedure_total_knee)
+jennifer_total_hip_count = len(df_jennifer_procedure_total_hip)
+count = 0
+for delay in df_jennifer_procedure_total_shoulder['Delay'].values:
+    if delay>=180:
+        count+=1
+
+jennifer_knee_count = 0
+for delay in df_jennifer_procedure_total_knee['Delay'].values:
+    #print(delay)
+    if delay>=180.0:
+        jennifer_knee_count = jennifer_knee_count + 1
+
+jennifer_hip_count = 0
+for delay in df_jennifer_procedure_total_hip['Delay'].values:
+    #print(delay)
+    if delay>=180.0:
+        jennifer_hip_count = jennifer_hip_count + 1
+        #print('for delay:' ,delay,'jennifer_hip_count:',jennifer_hip_count)
+
+
+#print('total hip',jennifer_total_hip_count) 
+#print('More_6 months_hip',jennifer_hip_count) 
+#print(df_jennifer_procedure_total_hip['Delay'].values)
+
+#UI
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Table Example')
+        self.setGeometry(100, 100, 500, 300)
+
+        # Create a table
+        table = QTableWidget(self)
+        table.setGeometry(50, 50, 1800, 900)
+        table.setRowCount(3)
+        table.setColumnCount(9)
+        table.setHorizontalHeaderLabels(["Total", "More than 6 months", "Percentage"])
+
+
+        if  jennifer_total_shoulder_count == 0:
+                total_shoulder_percentage =0
+        else:
+            total_shoulder_percentage = count/jennifer_total_shoulder_count
+        if  jennifer_total_hip_count == 0:
+                total_hip_percentage =0
+        else:
+            total_hip_percentage = count/jennifer_total_hip_count
+
+        # Add some sample data to the table
+        data = [["Alice", "25", "New York"],
+                ["Bob", "30", "London"],
+                [str(jennifer_total_shoulder_count), str(count), str("{:.2f}".format((total_shoulder_percentage)*100)),
+                 str(jennifer_total_knee_count),str(jennifer_knee_count),str("{:.2f}".format((jennifer_knee_count/jennifer_total_knee_count)*100)),
+                 str(jennifer_total_hip_count),str(jennifer_hip_count),str("{:.2f}".format((total_hip_percentage)*100))]]
+        for i, row in enumerate(data):
+            for j, item in enumerate(row):
+                table.setItem(i, j, QTableWidgetItem(item))
+                # Add new cells on top
+        header_cell1 = QTableWidgetItem("Martinez, Adriana")
+        header_cell1.setTextAlignment(Qt.AlignCenter)
+        table.setItem(0, 0, header_cell1)
+        table.setSpan(0, 0, 1, table.columnCount())  # span the cell across all columns
+
+        header_cell2 = QTableWidgetItem("Total Shoulder                                                           | Total Knee |                                                             Total Hip")
+        header_cell2.setTextAlignment(Qt.AlignCenter)
+        table.setItem(1, 0, header_cell2)
+        table.setSpan(1, 0, 1, table.columnCount())  # span the cell across all columns
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = MyWindow()
+    win.show()
+    sys.exit(app.exec_())
+
--- version_1.py
+++ version_1.py
@@ -1,29 +1,83 @@
 import tabula
 import os
 import pandas as pd
+import numpy as np
 
 # clear the terminal
 os.system('cls' if os.name == 'nt' else 'clear')
 
+#for the 1st page of the report
 pdf_path = './service_orthopedics_specialty_orthopedics-1.pdf'
 
-#tabula.convert_into(pdf_path,'output_2', output_format='csv', pages='all')
+#for all pages of the report
+pdf_path_all = './2022-04-30_onwards_record.pdf'
 
-tables = tabula.read_pdf(pdf_path, pages=1, multiple_tables=True,)
+pdf_path_2pages = './service_orthopedics_specialty_orthopedics-2.pdf'
+# Create an empty list to store the data frames for each page
+dfs = []
 
-name_col = tables[0]
+# Iterate over each page of the PDF and append the data frame to the list
+delay_time = []
+for page in range(1, len(tabula.read_pdf(pdf_path_all, pages='all'))+1):
+    tables = tabula.read_pdf(pdf_path_all, pages=page, multiple_tables=True)
+    if page == 1:
+        df = pd.concat(tables, ignore_index=True)
+        #remove the first 12 rows
+        df = df.iloc[12:]
+        df.columns = df.iloc[0]
+        df = df[1:]
+        df = df.drop(['R D Record No.', 'Last name, First name','Planned', 'Time Speciality',], axis=1)
+        dfs.append(df)
 
-df = name_col.iloc[12:]
-df.columns = df.iloc[0]
-df = df[1:]
+    if page != 1:
+         if isinstance(tables[0], pd.DataFrame):
+            # removes all the columns with no values in them
+            tables[0] = tables[0].dropna(axis=1, how='all')
+            #remove the first row
+            tables[0] = tables[0].drop(index=range(1, 2))
+            tables[0] = tables[0][1:]
+            dfs.append(tables[0])
 
-# Reset the index
-#df = df.reset_index(drop=True)
 
-print('this is name_col',name_col,'ends here.')
-print(df['Delay'])
-#print(name_col.keys())
-#print(name_col.iloc[1, 1])
+
+# Concatenate the data frames into a single data frame
+df = pd.concat(dfs, ignore_index=True)
+# Save the resulting data frame to a CSV file
+#print(df.columns.values)
+df.drop(["St. Mary's Hospital Center","Unnamed: 0","Unnamed: 3","Date:"], axis=1, inplace=True)
+
+
+# remove rows with all null values
+df = df.dropna(how='all')
+
+# replace NaN values with empty strings
+df = df.fillna('')
+df['Procedure, localization, technique'] = df['Procedure, localization, technique'].astype(str) + df['Unnamed: 1'].astype(str)
+df['Surgeon'] = df['Surgeon'].astype(str) + df['Unnamed: 2'].astype(str)
+
+#Beware of this as the time changes
+df['Entered'] = df['Entered'].astype(str) + df['2023-04-30'].astype(str)
+df['Delay'] = df['Delay'].astype(str) + df['Unnamed: 5'].astype(str)
+
+
+df = df.drop(['Unnamed: 1', 'Unnamed: 2','2023-04-30', 'Unnamed: 5',], axis=1)
+
+df = df.drop(df.columns[1], axis=1)
+
+df['Procedure, localization, technique'] = df['Procedure, localization, technique'].str.split(',').str[0]
+df = df.rename(columns={'Procedure, localization, technique': 'Procedure'})
+
+
+# convert the 'date' column to datetime format
+df['Entered'] = pd.to_datetime(df['Entered'])
+
+# filter the dataframe to only include dates after 2022-01-02
+filtered_df = df[df['Entered'] > '2022-04-30']
+print(len(filtered_df))
+
+
+#print(df.columns.values)
+#df.to_csv('./output_5.csv', index=False)
 
 
 
